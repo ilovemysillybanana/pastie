0c84ca866eed8b53a59f773d89fab991b9290f3a
Jose Manuel Tobar <ilovemysillybanana@gmail.com>
2016-01-17 19:07:38 -0500

    Added listing validations and removed those that caused name and language not be recorded

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,6 +4,13 @@ class Listing < ActiveRecord::Base
   belongs_to :user
   has_many :comments
 
+  validates :name, :presence => true,
+    :length => { :minimum => 1, :maximum => 25}
+  validates :language, :allow_nil => true,
+    :length => { :minimum => 1, :maximum => 25}
+  validates :code, :presence => true,
+    :length => { :minimum => 1, :maximum => 4000}
+
   before_create :randomize_id
   private
   def randomize_id
