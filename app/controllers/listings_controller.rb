3a4d9106425059519d5a38ae4abd68b3b2de48d6
Jose Manuel Tobar <ilovemysillybanana@gmail.com>
2016-01-18 12:22:31 -0500

    Removed user id refrence from create method in listing controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -40,7 +40,7 @@ class ListingsController < ApplicationController
   # POST /listings.json
   def create
     @listing = Listing.new(listing_params)
-    @listing.user_id = current_user.id
+    @listing.user = current_user
 
     respond_to do |format|
       if @listing.save
