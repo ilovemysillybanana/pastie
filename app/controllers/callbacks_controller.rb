fdb20543eeba5409e0cd46cb471ea00a8a3664ee
Jose Manuel Tobar <ilovemysillybanana@gmail.com>
2016-02-07 21:07:11 -0600

    Moved edit/delete buttons to listing show view

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,28 +1,36 @@
 class CallbacksController < Devise::OmniauthCallbacksController
   def github
     @user = User.from_omniauth(request.env["omniauth.auth"])
+    username_setter(@user)
     sign_in_and_redirect @user
   end
 
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
+    username_setter(@user)
     sign_in_and_redirect @user
   end
 
   def google_oauth2
     @user = User.from_omniauth(request.env["omniauth.auth"])
+    username_setter(@user)
     sign_in_and_redirect @user
   end
 
   def after_sign_in_path_for(user)
-    if user.sign_in_count == 0
-      user.username = "pastie#{user.id}"
+    if user.sign_in_count == 0 || user.sign_in_count == 1
+      #user.username = "pastie#{user.id}"
       flash[:success] = "Please make sure your username is changed through this form otherwise your username will be your ID."
       edit_user_registration_path
     else
       flash[:success] = "Welcome to pastie.xyz!"
       root_path
     end
+  end
+
+  private
+  def username_setter(user)
+    user.username = "pastie_user_#{user.id}"
   end
 
 end
