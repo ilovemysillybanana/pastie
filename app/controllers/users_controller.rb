6f795a7344cfbd3e84fa25d147aa6709bc340af1
Jose Manuel Tobar <ilovemysillybanana@gmail.com>
2016-03-14 01:28:28 -0500

    Ajaxified profile page for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
 class UsersController < ApplicationController
+  clear_respond_to
+  respond_to :html, :js, :json
+
   def show
     @user = User.find(params[:id])
 
     respond_to do |format|
         format.html # show.html.erb
+        format.js
         format.xml { render :xml => @user }
     end
   end
