81bc6d18319f6ff9af187b589b6cb150b7c618ee
Jose Manuel Tobar <ilovemysillybanana@gmail.com>
2016-04-12 23:07:35 -0500

    Set Precompile Assets to False

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Pastie
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.assets.initialize_on_precompile = false
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
